b70e89421896fcb75f3f6568ab666081f4b50b3f
Pierre <pierre.beylard@hotmail.fr>
2021-08-03 16:49:49 +0200

    refacto ML

--- titanic_pkg/ml.py
+++ titanic_pkg/ml.py
@@ -1,6 +1,9 @@
+from sklearn.metrics import classification_report, plot_confusion_matrix
+
 from titanic_pkg.preprocessing import Preprocessing
 from titanic_pkg.data import Data
 from titanic_pkg.pipeline_class import PipelineClass
+
 
 class ML:
     def __init__(self) :
@@ -8,12 +11,27 @@ class ML:
         self.X_train, self.X_test, self.y_train, self.y_test = Preprocessing().select_x_y()
         self.reg = PipelineClass().creation_pipeline()
 
-    def fit_predict(self):
-        self.reg.fit(self.X_train, self.y_train)
-        y_pred = self.reg.predict(self.X_test)
-        return y_pred
+    def model_fitting(self):
+        fitted_model = self.reg.fit(self.X_train, self.y_train)   
+        return fitted_model
 
-    def fit_score(self):
-        self.reg.fit(self.X_train, self.y_train)
-        score = self.reg.score(self.X_test, self.y_test)
-        return score
+    def model_predict_test(self, X = None):
+        fitted_model = self.model_fitting()
+        print(self.X_test)
+        print("############")
+        print(X)
+        if X is None : 
+            y_pred = fitted_model.predict(self.X_test)
+        else : 
+            y_pred = fitted_model.predict(X)
+        return y_pred, fitted_model
+        
+    def model_score(self):
+        y_pred, fitted_model = self.model_predict_test()
+        accuracy_score = fitted_model.score(self.X_test, self.y_test)
+        class_report = classification_report(self.y_test,y_pred)
+        conf_matrix = plot_confusion_matrix(fitted_model, self.X_test, self.y_test).figure_
+        return accuracy_score, class_report, conf_matrix
+        
+    def unseen_values(self,X=[]):
+        pass
