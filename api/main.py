6fceb7f7f13c853be1356ed4682b165dfde5c659
jessika martin <jessikahouete@orange.fr>
2021-08-04 16:55:03 +0200

    formulaire POST + API post + délire css

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 import uvicorn
 
 from titanic_pkg.ml import ML
@@ -11,18 +11,38 @@ app = FastAPI()
 async def get_root():
     return {'message': 'Welcome to titanic survivor prediction app'}
 
-@app.get('/prediction/{passenger}')
-async def survive_prediction(passenger:str):
-    list_params = passenger.split('&')
-    dict_params = {'pclass': [int(list_params[0])],
-                      'sex': [list_params[1]],
-                      'age': [float(list_params[2])],
-                      'fare': [float(list_params[3])],
-                      'embarked': [list_params[4]],
-                      'who': [list_params[5]],
-                      'alone': [bool(list_params[6])]
-                      }
-    X = pd.DataFrame(dict_params)
+
+@app.post("/passenger_dict/")
+async def passenger_dict(pclass: int = Form(...),
+                         sex: str = Form(...),
+                         age: float = Form(...),
+                         fare: float = Form(...),
+                         embarked: str = Form(...),
+                         who: str = Form(...),
+                         alone: bool = Form(...)):
+    passengers = {
+        'pclass': [pclass],
+        'sex': [sex],
+        'age': [age],
+        'fare': [fare],
+        'embarked': [embarked],
+        'who': [who],
+        'alone': [alone]
+    }
+    X = pd.DataFrame(passengers)
+    prediction = ML()
+    pred = prediction.model_predict_test(X)
+    if pred[0][0] == 0:
+        result = 'No'
+    else:
+        result = 'Yes'
+    return {'Did you survive ?': result}
+    # return f'/prediction/{passengers}'
+
+
+@app.get('/prediction/{passengers}')
+async def survive_prediction(passengers:str):
+    X = pd.DataFrame(passengers)
     prediction = ML()
     pred = prediction.model_predict_test(X)
     if pred[0][0] == 0:
@@ -31,7 +51,6 @@ async def survive_prediction(passenger:str):
         result = 'Yes'
     return {'Did you survive ?': result}
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
-
-
